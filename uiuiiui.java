efac1cc907c3390dfa637e0caf1a0ae52c937549
Adrian <superlaya50@gmail.com>
2020-01-14 17:52:15 +0100

    gfxgrs

---
+++ uiuiiui.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class uiuiiui here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class uiuiiui
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class uiuiiui
+     */
+    public uiuiiui()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
