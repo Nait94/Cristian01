5b464e80d461957f07b4d8c752f086e8da74124b
Cristian <cristianj503@gmail.com>
2020-01-14 17:03:53 +0100

    Intercambio inicial de proyecto

---
+++ Lesson1Github.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Lesson1Github here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Lesson1Github
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Lesson1Github
+     */
+    public Lesson1Github()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
